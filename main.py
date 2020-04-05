0a445c9b5f18f672390f01113c9fb2b5dcd67307
itsSayantan <sayantan.ghosh03@gmail.com>
2020-04-05 14:16:00 +0530

    1. users template file modified. 2. function created to create users.csv file 3. minor changes in main.py.

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from src.utils.common import CommonUtils
 from src.state.State import State
+from src.utils.common import CommonUtils
+from src.utils.data import DataOperations
 
+
+class Main:
+    def initialize_application():
+        # try to create the data files
+        DataOperations.create_user_dataset()
+        # initiate the login flow
+
+
+Main.initialize_application()
 c = CommonUtils()
 
-c.greet_user()
+# c.greet_user()
 
-while input('Type a command and press ENTER: ') != 'exit':
+while input('\nType a command and press ENTER: ') != 'exit':
     State.set_state({'name': 'Sayantan', 'age': 25})
     print(State.get('name'))
